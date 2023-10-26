22b6f892c208eb383455427ef00603254f497f78
Miguel Angel Pulido Quinche <140007642+M1-gu3-lo@users.noreply.github.com>
2023-10-25 20:43:25 -0500

    adas

---
+++ bolipola/bolipola/db.py
@@ -0,0 +1,17 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_bolipola',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
+    }
+}
